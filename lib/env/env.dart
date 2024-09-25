7f38fb72c0193981572951534a08e66243bc9af7
0xcplus <choi1058cjf@gmail.com>
2024-09-26 05:03:03 +0900

    delet web and windws files temporally

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,8 +1,8 @@
 import 'package:envied/envied.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+part 'env.g.dart';
 
 @Envied(path: ".env")
 abstract class Env {
   @EnviedField(varName: 'OPEN_AI_API_KEY') // the .env variable.
-  static String get apiKey => dotenv.env['OPEN_AI_API_KEY']??'dummy_key';
+  static const String apiKey = _Env.apiKey;
 }
