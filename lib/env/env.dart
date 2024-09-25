38dd7117c8b4adf25cf493db2c8c826a8d650e0e
0xcplus <choi1058cjf@gmail.com>
2024-09-25 18:00:33 +0900

    env.dart error fixed

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -4,5 +4,5 @@ part 'env.g.dart';
 @Envied(path: ".env")
 abstract class Env {
   @EnviedField(varName: 'OPEN_AI_API_KEY') // the .env variable.
-  static const apiKey = _Env.apiKey;
+  static const String apiKey = _Env.apiKey;
 }
