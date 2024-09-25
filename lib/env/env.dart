c67854e618b4260e6dfb781298498f37cfe48d3f
0xcplus <choi1058cjf@gmail.com>
2024-09-26 06:03:28 +0900

    hello?

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,8 +1,8 @@
 import 'package:envied/envied.dart';
-part 'env.g.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 @Envied(path: ".env")
 abstract class Env {
   @EnviedField(varName: 'OPEN_AI_API_KEY') // the .env variable.
-  static const apiKey = _Env.apiKey;
+  static String get apiKey => dotenv.env['OPEN_AI_API_KEY']??'dummy_key';
 }
