cb1f7ae33e2842f13c3df2c8249792e93b7da357
0xcplus <choi1058cjf@gmail.com>
2024-09-26 23:38:15 +0900

    last version

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,19 +1,19 @@
 import 'package:envied/envied.dart';
-//import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-part 'env.g.dart';
+//part 'env.g.dart';
 
-@Envied(path:"assets/.env")
+@Envied(path:"assets/config/.env")
 abstract class Env {
   @EnviedField(varName: 'OPEN_AI_API_KEY')
-  static const String apiKey = _Env.apiKey;
+  //static const String apiKey = _Env.apiKey;
 
-  /*static String get apiKey {
+  static String get apiKey {
     try {
       return dotenv.env['OPEN_AI_API_KEY'] ?? 'dummy_key';     
     } catch (e) {
       print('Error loading API key: $e');
       return 'fatal error;';
     }
-  }*/
+  }
 }
