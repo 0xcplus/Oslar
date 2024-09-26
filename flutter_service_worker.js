989e95ad42494bd84a6deddbe0e55e2ca0de97d1
0xcplus <0xcplus@users.noreply.github.com>
2024-09-26 18:23:09 +0000

    deploy: 0468110253b376a13e19ba7222f4a8c6e231ddab

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.json": "ca712d30b392427bb3e429f1be445bf
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"flutter_bootstrap.js": "e0d21665c67474921d45b2d2393f5a26",
+"flutter_bootstrap.js": "4d269292ed728acc7c7087a2a2b87e89",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "index.html": "abb4e9a2d5a0f2241477ab1228b5a323",
