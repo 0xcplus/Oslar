256cb6b4b407c210760edb9dc6b1b97512bbcea5
0xcplus <choi1058cjf@gmail.com>
2024-09-27 03:25:17 +0900

    credentials

--- api/getApiKey.js
+++ api/getApiKey.js
@@ -2,9 +2,10 @@ export default function handler(req, res) {
   const apiKey = process.env.YOUR_API_KEY; // 환경 변수에서 API 키 가져오기
 
   // CORS 헤더 설정
-  res.setHeader('Access-Control-Allow-Origin', '*'); // 모든 출처 허용
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // 허용할 HTTP 메서드
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // 허용할 헤더
+  res.setHeader('Access-Control-Allow-Origin', 'https://0xcplus.github.io'); // 특정 출처 허용
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   // OPTIONS 요청 처리
   if (req.method === 'OPTIONS') {
@@ -13,5 +14,9 @@ export default function handler(req, res) {
   }
 
   // GET 요청 처리
-  res.status(200).json({ apiKey });
+  if (req.method === 'GET') {
+    res.status(200).json({ apiKey }); // API 키를 반환
+  } else {
+    res.status(405).end(); // 허용되지 않은 메서드에 대한 405 응답
+  }
 }
